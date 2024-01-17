553bcf44578c26367a1f1ea3c9c688492fea7fd2
William Rena <wilyamx@gmail.com>
2024-01-17 11:07:22 +0800

    Fix initialization of theme

--- app/auth/AppContextProvider.js
+++ app/auth/AppContextProvider.js
@@ -1,14 +1,19 @@
 import React, { createContext, useState } from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { useSelector } from 'react-redux';
 
-import { LightBlue } from '../config/Themes';
+import { DarkTheme, LightTheme } from '../config/Themes';
 
 export const AppContext = createContext(null);
 
 // https://medium.com/@SeishinBG/dynamic-switching-of-themes-in-react-native-app-the-funky-way-with-hooks-48b57ab62a79
 
 export default ({ children }) => {
-    const [theme, changeTheme] = useState(LightBlue);
+    // redux
+    const colorScheme = useSelector(state => state.theme.colorScheme);
+
+    // theme
+    const [theme, changeTheme] = useState(colorScheme === 'dark' ? DarkTheme : LightTheme);
 
     return (
         <AppContext.Provider value={{theme: theme, changeTheme}}>
@@ -17,4 +22,4 @@ export default ({ children }) => {
             </PaperProvider>
         </AppContext.Provider>
     )
-}
+};
