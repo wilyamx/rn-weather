dbd6e5dc7ecf1bd7338204c307a9e7fdbaaaaeb2
William Rena <wilyamx@gmail.com>
2024-02-10 13:57:14 +0800

    Enhance tabbar icons

--- app/navigations/AppNavigator.js
+++ app/navigations/AppNavigator.js
@@ -9,6 +9,7 @@ import { useTranslation } from 'react-i18next';
 import HomeScreen from '../screens/HomeScreen';
 import LocationsScreen from '../screens/LocationsScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+
 import LOG from '../utility/logger';
 
 const Tab = createBottomTabNavigator();
@@ -39,7 +40,12 @@ function AppNavigator(props) {
                     component={HomeScreen}
                     options={{ 
                         headerShown: false,
-                        tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="home" color={color} size={size} />
+                        tabBarIcon: ({ focused, color, size }) =>
+                            <MaterialCommunityIcons
+                                name="home"
+                                color={color}
+                                size={focused ? 35 : size}
+                            />
                     }}
                     listeners={({ navigation }) => ({ tabPress: () => {
                         LOG.info("[AppNavigator]/tabPress", "HomeScreen");
@@ -55,7 +61,12 @@ function AppNavigator(props) {
                     component={LocationsScreen}
                     options={{ 
                         headerShown: false,
-                        tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="map-marker" color={color} size={size} />
+                        tabBarIcon: ({ focused, color, size }) => 
+                            <MaterialCommunityIcons
+                                name="map-marker"
+                                color={color}
+                                size={focused ? 35 : size}
+                            />
                     }}
                 />
                 <Tab.Screen
@@ -63,7 +74,12 @@ function AppNavigator(props) {
                     component={SettingsScreen}
                     options={{ 
                         headerShown: false,
-                        tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="account" color={color} size={size} />
+                        tabBarIcon: ({ focused, color, size }) => 
+                            <MaterialCommunityIcons
+                                name="account"
+                                color={color}
+                                size={focused ? 35 : size}
+                            />
                     }}
                     
                 />
