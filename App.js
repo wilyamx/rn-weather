017b469caa5f8d18d7c4a9fa447163ca8fd71639
William Rena <wilyamx@gmail.com>
2024-02-11 08:17:37 +0800

    Splash screen waiting time if needed

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
+import * as SplashScreen from 'expo-splash-screen';
 
 import AppContextProvider from './app/auth/AppContextProvider';
 import AppNavigator from './app/navigations/AppNavigator';
 import reduxStore from "./app/redux/store";
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
   const { store, persistor } = reduxStore();
+
+  const prepare = async () => {
+    try {
+      console.log("[App]/useEffect/prepare");
+      // delay simulation
+      //await new Promise(resolve => setTimeout(resolve, 5000));
+      await SplashScreen.hideAsync();
+      setIsReady(true);
+    } catch (error) {    
+      console.log("[App]/useEffect/prepare/error", error);
+    }
+  };
+
+  // Keep the splash screen visible while we fetch resources
+  SplashScreen.preventAutoHideAsync();
+
+  useEffect(() => {
+    prepare();
+  }, []);
+
+  if (!isReady) return null;
 
   return (
     <Provider store={store}>
